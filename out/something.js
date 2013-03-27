5bbe42c83c6ace1bca0546d741524333d8899b3f
Daniel Ribeiro <danielrb@pagerduty.com>
2013-03-26 20:52:13 -0700

    counting scores

--- out/something.js
+++ out/something.js
@@ -1,12 +1,26 @@
 goog.provide('whackmole');
 goog.require('cljs.core');
+whackmole.score = cljs.core.atom.call(null,0);
+whackmole.jq = (function jq(arg){
+return jQuery(arg);
+});
+whackmole.log = (function log(arg){
+return console.log(arg);
+});
 whackmole.bind_events = (function bind_events(){
-return jQuery(".hole").click((function (this$){
-return jQuery(this$.target).removeClass("holeWithMole");
+return whackmole.jq.call(null,".hole").click((function (this$){
+if(cljs.core.truth_(whackmole.jq.call(null,this$.target).hasClass("holeWithMole")))
+{whackmole.jq.call(null,this$.target).removeClass("holeWithMole");
+cljs.core.swap_BANG_.call(null,whackmole.score,cljs.core.inc);
+whackmole.log.call(null,"the score is");
+return whackmole.log.call(null,cljs.core.deref.call(null,whackmole.score));
+} else
+{return null;
+}
 }));
 });
 whackmole.mole_at = (function mole_at(n){
-return jQuery(jQuery(".hole").get(n)).addClass("holeWithMole");
+return whackmole.jq.call(null,whackmole.jq.call(null,".hole").get(n)).addClass("holeWithMole");
 });
 jQuery(whackmole.bind_events);
 setInterval((function (){
--- src-cljs/something.cljs
+++ src-cljs/something.cljs
@@ -5,15 +5,23 @@
 ;  )
 
 
-(def score 0)
+(def score (atom 0))
 
 (defn jq [arg] (js/jQuery arg))
 
+(defn log [arg] (.log js/console arg))
+
 (defn bind-events [] (.click (jq ".hole")
                        (fn [this]
-                         (.removeClass (jq (.-target this)) "holeWithMole" )
+                         (when (.hasClass (jq (.-target this)) "holeWithMole" )
+                          (do
+                            (.removeClass (jq (.-target this)) "holeWithMole" )
+                            (swap! score inc)
+                            (log "the score is")
+                            (log @score)
+                            )
 
-                         )))
+                         ))))
 
 (defn mole-at [n] (.addClass (jq (.get (jq ".hole") n)) "holeWithMole" ))
 
