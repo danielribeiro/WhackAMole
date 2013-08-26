82104145d1b6c5f4f16857224266cda6627da6a8
Daniel Ribeiro <danielrb@pagerduty.com>
2013-08-25 20:54:42 -0700

    confiruging compiler

--- project.clj
+++ project.clj
@@ -5,4 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :plugins [[lein-cljsbuild "0.3.2"]]
+  :cljsbuild {
+    :builds [{
+        ; The path to the top-level ClojureScript source directory:
+        ; The standard ClojureScript compiler options:
+        ; (See the ClojureScript compiler documentation for details.)
+        :compiler {
+          :optimizations :whitespace
+          :pretty-print true}}]}
   )
